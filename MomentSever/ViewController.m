8e293ac157333f1bee20e7643397cac245dbff83
yanglle <wjzy0918@163.com>
2017-03-30 21:16:34 +0800

    讲脚本写入程序

--- MomentSever/ViewController.m
+++ MomentSever/ViewController.m
@@ -12,7 +12,9 @@
 #import "ViewController.h"
 #import "GCDAsyncSocket.h"
 #import "Core/HTTPConnection.h"
-
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <ifaddrs.h>
 @implementation ViewController
 @synthesize socket = _socket;
 @synthesize url = _url;
@@ -99,7 +101,8 @@ BOOL isReady=0;
         NSLog(@"[Server] Started at Port: %hu",self.socket.localPort);
         isConnected=1;
         _startBtn.title=@"结束";
-        [self appendToMyTextView:@"[Server] Started"];
+        NSString *welcome= [@"[Server] Started at " stringByAppendingString:[self localIPAddress]];
+        [self appendToMyTextView:welcome];
     }else{
         //NSLog(@"[Server] Started error: %@",&error);
     }
@@ -221,6 +224,30 @@ BOOL isReady=0;
     return strReturnFromShell;
     
 }
+
+- (NSString *)localIPAddress
+{
+    NSString *localIP = nil;
+    struct ifaddrs *addrs;
+    if (getifaddrs(&addrs)==0) {
+        const struct ifaddrs *cursor = addrs;
+        while (cursor != NULL) {
+            if (cursor->ifa_addr->sa_family == AF_INET && (cursor->ifa_flags & IFF_LOOPBACK) == 0)
+            {
+                //NSString *name = [NSString stringWithUTF8String:cursor->ifa_name];
+                //if ([name isEqualToString:@"en0"]) // Wi-Fi adapter
+                {
+                    localIP = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)cursor->ifa_addr)->sin_addr)];
+                    break;
+                }
+            }
+            cursor = cursor->ifa_next;
+        }
+        freeifaddrs(addrs);
+    }
+    return localIP;
+}
+
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
