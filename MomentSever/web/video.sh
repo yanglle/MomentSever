94614eab04b7b0a5bf0231ebd2be153b72b66e0e
yanglle <wjzy0918@163.com>
2017-04-08 13:14:41 +0800

    修复 图片融合错误

--- MomentSever/web/video.sh
+++ MomentSever/web/video.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd /Users/"$USER"/Desktop/photos;
 i=1; for x in *; do mv $x $i.jpg; let i=i+1; done
-/usr/local/bin/ffmpeg -framerate 15 -i /Users/"$USER"/Desktop/photos/%d.jpg -c libx264 -vf transpose=1 -s /Users/"$USER"/Desktop/output/"`date +%m%d_%H%M%S`".mp4;
+/usr/local/bin/ffmpeg -framerate 15 -i /Users/"$USER"/Desktop/photos/%d.jpg -c libx264 -vf transpose=1  /Users/"$USER"/Desktop/output/"`date +%m%d_%H%M%S`".mp4;
 mkdir /Users/"$USER"/Desktop/output/"`date +%m%d_%H%M%S`";
 mv /Users/"$USER"/Desktop/photos/* /Users/"$USER"/Desktop/output/"`date +%m%d_%H%M%S`";
 open /Users/"$USER"/Desktop/output;
